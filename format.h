7b2ecaef3472408bf1dd2b03003bdd5b6096ac50
loxxous <lucasgmarsh@gmail.com>
2017-02-21 13:57:42 -0600

    Update format.h

--- format.h
+++ format.h
@@ -13,23 +13,34 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <pthread.h>
+#include "sys_detect.h"
 
-#define BLOCKSIZE 		4 << 20
-//#define MIN_BLOCKSIZE 	1 << 20		// For variable block size support, not yet implemented
-//#define MAX_BLOCKSIZE 	100 << 20
+#define BLOCKSIZE 	4 << 20
 #define MAX_THREADS	getCoreCount()
 #define MIN_THREADS	1
 #define DEFAULT_THREADS ((getCoreCount() == 1) ? 1 : getCoreCount() - 1)
-#define HBITS 19
-#define ELEMENTS 1 << HBITS
+#define HBITS 		19
+#define ELEMENTS 	1 << HBITS
 
 typedef unsigned char byte;
+
 struct match{
-	int pos;
+	unsigned int pos;
 	int ctx;
 };
+
 static inline int hash(int word){
 	return (word * 0x9E3779B1) >> (32 - HBITS);
+}
+
+struct buffer{
+	byte *buf;
+	int *size;
+};
+
+void Error(const char *string)
+{
+	printf ("\n Error: %s \n", string);
 }
 
 #endif // FORMAT_H
