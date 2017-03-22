1fe38bcaecd37fdd36750e9b15c4f861c1413b72
loxxous <lucasgmarsh@gmail.com>
2017-03-21 19:52:59 -0500

    1GB block support!

--- format.h
+++ format.h
@@ -15,17 +15,20 @@
 #include <pthread.h>
 #include "sys_detect.h"
 
-#define DEFAULT_BLOCKSIZE 	16 << 20
+#define DEFAULT_BLOCKSIZE 	4 << 20
 #define MIN_BLOCKSIZE 		1 << 20
-#define MAX_BLOCKSIZE 		256 << 20	// This is limited by the LZ77 precompressor specs
+#define MAX_BLOCKSIZE 		1000 << 20
 #define MAX_THREADS		getCoreCount()
 #define MIN_THREADS		1
 #define DEFAULT_THREADS 	((getCoreCount() == 1) ? 1 : getCoreCount() - 1)
-#define HBITS 			20
-#define LZ_ELEMENTS 		1 << HBITS	// The size of the LZ hash table
+#define DUPE_HBITS 			20	// The size of the LZ dedupe table
+#define SHORT_HBITS 			18	// The size of the LZ short match table
+#define LZ_DUPE_ELEMENTS 		1 << DUPE_HBITS	
+#define LZ_SHORT_ELEMENTS 		1 << SHORT_HBITS
 #define BWT_UNITS 		840		// The amount of independant parallel units that can process the BWT block
 
-const char Magic[]="JAM";
+static const char Magic[]="JAM";
+static const char MagicLength = 3;
 typedef int Index;
 
 struct Buffer
@@ -34,37 +37,15 @@ struct Buffer
 	int *size;
 };
 
-struct Match
-{
-	int pos;
-	int ctx;
-};
-
-static inline int Hash(int word)
-{
-	return (word * 0x9E3779B1) >> (32 - HBITS);
-}
-
 static  pthread_mutex_t lock; 
-int Global_Error_Count = 0;
 
-/*
-	When debugging mode is disabled it allows the process to kill itself during an error. 
-	Enabling debugging displays error information and prevents too many errors from occuring.
-*/
 void Error(const char *string)
 {
 	pthread_mutex_init(&lock, NULL);
 	pthread_mutex_lock(&lock);
-	
-		printf ("\n Error: %s \n", string);
-		#ifdef NDEBUG
-			exit(1);
-		#endif
-		Global_Error_Count++;
-		if(Global_Error_Count > 8){ printf ("\n Aborting operation: Too many errors! \n"); exit(2); }
-		
+		printf ("\n Error: %s \n", string);		
 	pthread_mutex_unlock(&lock);
+	exit(1);
 }
 
 #endif // FORMAT_H
