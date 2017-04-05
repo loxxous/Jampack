7549827ee2774461c9fec70db1a50b940cbaf2b1
loxxous <lucasgmarsh@gmail.com>
2017-04-04 20:50:48 -0500

    Compression improvements

--- ans.cpp
+++ ans.cpp
@@ -1,13 +1,14 @@
 /*********************************************
 * ANS compression and decompression code
-*
-* It is MTF + semi-static inherited order-1.
+* MTF variant + RLE0 + rANS
 **********************************************/
 #ifndef ANS_H
 #define ANS_H
 
 #include "rans_byte.h"
 #include "model.h"
+#include "rank.cpp"
+#include "rle.cpp"
 
 class ANS
 {
@@ -15,17 +16,16 @@ public:
 	void encode(Buffer Input, Buffer Output);
 	void decode(Buffer Input, Buffer Output, int Threads);
 private:
-	int StackSize = 128 << 10; 	// Desired block size
-	int SBufSize = (StackSize * 2); // Size of temporary buffer prior to merging
-	void ThreadedDecodeLoad(Buffer Input, Buffer Output, int in_p, int out_p, int clen, int olen, Model *stats);
+	static const int StackSize = 128 << 10; 	// Desired block size
+	static const int SBufSize = (StackSize * 2); // Size of temporary buffer prior to merging
 
 	class Parallel_ANS
 	{
 	private:
-		Buffer Input; Buffer Output; int in_p; int out_p; int clen; int olen; Model *stats;
+		Buffer Input; Buffer Output; int in_p; int out_p; int clen; int olen; int rlen; Model *stats;
 		
 	public:
-		void Load (Buffer _Input, Buffer _Output, int _in_p, int _out_p, int _clen, int _olen, Model *_stats)
+		void Load (Buffer _Input, Buffer _Output, int _in_p, int _out_p, int _clen, int _olen, int _rlen, Model *_stats)
 		{
 			Input = _Input;
 			Output = _Output;
@@ -33,11 +33,13 @@ private:
 			out_p = _out_p;
 			clen = _clen;
 			olen = _olen;
+			rlen = _rlen;
 			stats = _stats;
 		}
 
 		void Threaded_Decode()
 		{
+			unsigned short *rlebuf = (unsigned short*)calloc(StackSize, sizeof(unsigned short)); if(rlebuf == NULL) Error("Couldn't allocate rle buffer!");
 			stats->SetDecodeTable();
 			uint8_t *rans_begin = &Input.block[in_p];
 			uint8_t* ptr = rans_begin;
@@ -46,13 +48,12 @@ private:
 			RansDecInit(&R[1], &ptr);
 			RansDecInit(&R[2], &ptr);
 			RansDecInit(&R[3], &ptr);
-			int sptr = 0;
-			for(sptr = 0; sptr < olen; sptr++)
+			for(int sptr = 0; sptr < rlen; sptr++)
 			{
 				RansState X = R[0];
 				int range = RansDecGet(&X, stats->ProbBits);
-				uint32_t s = stats->DecGetSym(range);
-				Output.block[out_p+sptr] = s;
+				unsigned short s = stats->DecGetSym(range);
+				rlebuf[sptr] = s;
 				RansDecAdvanceSymbol(&X, &ptr, stats->DecGetNext(s), stats->ProbBits);
 				R[0] = R[1];
 				R[1] = R[2];
@@ -60,19 +61,26 @@ private:
 				R[3] = X;
 			}
 			stats->Clear();
+
+			RLE *rle0 = new RLE(); if(rle0 == NULL) Error("Couldn't allocate rle0!");
+			rle0->decode(rlebuf, &Output.block[out_p], &rlen, olen);
+			delete rle0;
+			free(rlebuf);
 			
-			Postcoder *mtf = new Postcoder(); if(mtf == NULL) Error("Couldn't allocate postcoder");
-			mtf->decode(&Output.block[out_p], olen);
-			delete mtf;
+			Postcoder *rank = new Postcoder(); if(rank == NULL) Error("Couldn't allocate postcoder!");
+			rank->decode(&Output.block[out_p], olen);
+			delete rank;
 		}
 		
-		static void* Launch(void* object){
+		static void* Launch(void* object)
+		{
 			Parallel_ANS* obj = reinterpret_cast<Parallel_ANS*>(object);
 			obj->Threaded_Decode();
 			return NULL;
 		}
 		
-		void pThread_ANS_Decode(pthread_t *thread){
+		void pThread_ANS_Decode(pthread_t *thread)
+		{
 			int err = pthread_create(thread, NULL, &Parallel_ANS::Launch, this); 
 			if(err) Error("Unable to create thread!");
 		}
@@ -81,10 +89,12 @@ private:
 
 void ANS::encode(Buffer Input, Buffer Output)
 {
-	Postcoder *mtf = new Postcoder(); if(mtf == NULL) Error("Couldn't allocate postcoder!");
+	Postcoder *rank = new Postcoder(); if(rank == NULL) Error("Couldn't allocate postcoder!");
+	RLE *rle0 = new RLE(); if(rle0 == NULL) Error("Couldn't allocate rle0!");
 	Model *stats = new Model(); if(stats == NULL) Error("Couldn't allocate model!");
-	RansEncSymbol *stack = (RansEncSymbol*)calloc(StackSize, sizeof(RansEncSymbol));
-	unsigned char *tmp = (unsigned char*)calloc(SBufSize, sizeof(unsigned char));
+	RansEncSymbol *stack = (RansEncSymbol*)calloc(StackSize, sizeof(RansEncSymbol)); if(stack == NULL) Error("Couldn't allocate rans stack!");
+	unsigned short *rlebuf = (unsigned short*)calloc(StackSize, sizeof(unsigned short)); if(rlebuf == NULL) Error("Couldn't allocate rle buffer!");
+	unsigned char *tmp = (unsigned char*)calloc(SBufSize, sizeof(unsigned char)); if(tmp == NULL) Error("Couldn't allocate temporary buffer!");
 	int in_p = 0;
 	int out_p = 0;
 	
@@ -92,16 +102,15 @@ void ANS::encode(Buffer Input, Buffer Output)
 	{
 		// Gather statistics for the block
 		int len = ((in_p+StackSize) < *Input.size) ? StackSize : (*Input.size - in_p);
-		
-		mtf->encode(&Input.block[in_p], len);
-		
-		//stats->Build(&Input.block[in_p], len);
-		stats->BuildRLE0(&Input.block[in_p], len);
+		rank->encode(&Input.block[in_p], len);
+		int rlen = len;
+		rle0->encode(&Input.block[in_p], rlebuf, &rlen);
+		stats->Build(rlebuf, rlen);
 		stats->SetEncodeTable();
 		
 		int sptr = 0;
-		for(; sptr < len; sptr++)
-			stack[sptr] = stats->EncGetRange((int)Input.block[in_p+sptr]);
+		for(; sptr < rlen; sptr++)
+			stack[sptr] = stats->EncGetRange(rlebuf[sptr]);
 		
 		RansState R[4];
 		RansEncInit(&R[0]);
@@ -127,7 +136,7 @@ void ANS::encode(Buffer Input, Buffer Output)
 		rans_begin = ptr;
 		int csize = &tmp[SBufSize] - rans_begin;
 		
-		out_p += stats->WriteHeader(&Output.block[out_p], &len, &csize);
+		out_p += stats->WriteHeader(&Output.block[out_p], &len, &csize, &rlen);
 		
 		// Merge the buffer to the output stream
 		for(int k = 0; k < csize; k++) 
@@ -141,8 +150,10 @@ void ANS::encode(Buffer Input, Buffer Output)
 	*Output.size = out_p;
 	free(stack);
 	free(tmp);
-	delete mtf;
+	free(rlebuf);
+	delete rank;
 	delete stats;
+	delete rle0;
 }
 
 void ANS::decode(Buffer Input, Buffer Output, int Threads)
@@ -158,11 +169,12 @@ void ANS::decode(Buffer Input, Buffer Output, int Threads)
 	{
 		int olen = 0;
 		int clen = 0;
+		int rlen = 0;
 		int s = 0;
 		while ((in_p < *Input.size) && (s < Threads))
 		{
-			in_p += stats[s].ReadHeader(&Input.block[in_p], &olen, &clen, StackSize);
-			pANS[s].Load(Input, Output, in_p, out_p, clen, olen, &stats[s]);
+			in_p += stats[s].ReadHeader(&Input.block[in_p], &olen, &clen, &rlen, StackSize);
+			pANS[s].Load(Input, Output, in_p, out_p, clen, olen, rlen, &stats[s]);
 			in_p += clen;
 			out_p += olen;
 			s++;
