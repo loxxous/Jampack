001b39d5b41f1df98dc6c87eb103ff0281a6c1ab
loxxous <lucasgmarsh@gmail.com>
2017-04-14 15:07:59 -0500

    Update bwt.hpp

--- bwt.hpp
+++ bwt.hpp
@@ -7,28 +7,26 @@
 #include "format.hpp"
 #include "divsufsort.hpp"
 
-class BlockSort
+namespace BlockSort
 {
-	public:
-	void ForwardBWT(Buffer Input, Buffer Output, Index *Indicies);
-	void InverseBWT(Buffer Input, Buffer Output, Index *Indicies, int Threads);
-	
-	private:
-	class Parallel_BWT
+	class BWT
 	{
-		private:
-		unsigned char *BWT; unsigned char *T; int Step; Index *p; Index Idx; Index* MAP; int *Offset; int Start; int End; int N_Units;
-		
 		public:
-		void Load(unsigned char *_BWT, unsigned char *_T, int _Step, Index *_p, Index _Idx, Index* _MAP, int *_Offset, int _Start, int _End, int _NU);
-		void Threaded_Inversion();
-		#ifdef __CUDACC__
-		__device__ void CUDA_Threaded_Inversion();
-		#endif
+		void ForwardBWT(Buffer Input, Buffer Output, Index *Indicies);
+		void InverseBWT(Buffer Input, Buffer Output, Index *Indicies, int Threads);
+		
+		class ParallelBWT
+		{
+			private:
+			unsigned char *BWT; unsigned char *T; int Step; Index *p; Index Idx; Index* MAP; int *Offset; int Start; int End; int N_Units;
+			
+			public:
+			void Load(unsigned char *_BWT, unsigned char *_T, int _Step, Index *_p, Index _Idx, Index* _MAP, int *_Offset, int _Start, int _End, int _NU);
+			void ThreadedInversion();
+		};
 	};
-	public:
 	#ifdef __CUDACC__
-	__host__ __device__ void CUDA_Threaded_Launch(Parallel_BWT* pBWT, int Threads);
+	__global__ void CUDAInverse(int Threads, unsigned char *BWT, unsigned char *T, int Step, Index *p, Index Idx, Index* MAP, int *Offset);
 	#endif
 };
 #endif // BWT_H
