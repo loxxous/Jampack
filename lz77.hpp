8a0f37d3775f7ac98e007ea36d63d4cf75814125
loxxous <lucasgmarsh@gmail.com>
2017-09-19 18:38:37 -0500

    Update lz77.hpp

--- lz77.hpp
+++ lz77.hpp
@@ -1,33 +1,42 @@
 /*********************************************
-* LZ77 compression and decompression header
+* Lz77 compression and decompression header
+*
+* In Jampack lz77 is used as a precompressor for complex data,
+* it first finds all the best matches and buffers them up in 64 KB chunks,
+* then it performs a heuristic analysis on the parsed tokens to find the representaition 
+* which doesn't hurt context before encoding.
 **********************************************/
 
 #ifndef LZ77_H
 #define LZ77_H
 
 #include "format.hpp"
+#include "divsufsort.hpp"
 #include "utils.hpp"
+#include "cyclichhm.hpp"
 
 class Lz77
 {
 public:
-	void Compress(Buffer Input, Buffer Output, int minmatch);
+	Lz77();
+	~Lz77();
+	void Compress(Buffer Input, Buffer Output, Options Opt);
 	void Decompress(Buffer Input, Buffer Output);
 private:
-	bool Compressible(int look_ahead, int match_len, int offset, int minmatch);
-	const unsigned int constants[4] = 
-	{0xff >> 1, (0xffff >> 2) + (0xff >> 1), 
-	(0xffffff >> 3) + (0xffff >> 2) + (0xff >> 1), 
-	(0xffffffff >> 4) + (0xffffff >> 3) + (0xffff >> 2) + (0xff >> 1)};
-	
-	inline int HashLong(int ctx);
-	inline int Hash32(unsigned char *p);
-	
-	// Basic structure for comparing large contexts
-	struct MatchLong
-	{
-		int pos;
-		int ctx;
-	};
+	Utils *Leb;
+	void FastCopy(unsigned char *dest, unsigned char *src, Index length);
+	void FastCopyOverlap(unsigned char *dest, unsigned char *src, Index length);
+	float Compressible(Index match, Index literal, Index offset);
+	Index WriteToken(unsigned char *out, Index match, Index literal, Index offset);
+	Index ReadToken(unsigned char *in, Index *match, Index *literal, Index *offset);
+	inline unsigned int Load32(unsigned char *ptr);
+	inline unsigned int Hash32(unsigned char *ptr);
+	inline unsigned int Hash(unsigned int v);
+	const int MIN_MATCH = 4;
+	const int DUPE_MATCH = 256;
+	const int TOKEN_BUFFER_BITS = 16; 
+	const int TOKEN_BUFFER_SIZE = 1 << TOKEN_BUFFER_BITS; // 64 KB token chunk
+	const int HASH_BITS = 22;
+	const int HASH_SIZE = 1 << HASH_BITS;
 };
 #endif // LZ77_H
