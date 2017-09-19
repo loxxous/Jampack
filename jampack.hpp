b9b45f7b4fcc2f88f337cc203fd4b60850191ed2
loxxous <lucasgmarsh@gmail.com>
2017-09-19 18:52:35 -0500

    Update jampack.hpp

--- jampack.hpp
+++ jampack.hpp
@@ -19,35 +19,32 @@ class Jampack
 	Buffer Output;
 	
 	private:
-	BlockSort::Bwt *Bwt;
-	Lz77 *Lz;
-	Ans *Entropy;
-	Filters *Filter;
-	Lpx *LocalModel;
+	BlockSort::Bwt *Bwt;			// Burrows wheeler transform
+	Lz77 *Lz;				// Lz77 codec for special cases
+	Ans *Entropy;				// Structured rANS coder
+	Filters *Filter;			// Generic filters 
+	Lpx *LocalModel;			// Local bijective low-order match model
+	Checksum *Chk;				// Fast checksum implementation
+	Options Option;				// Optional compression arguments are passed through the 'Options' type
 	Index BlockSize;
-	Index *Indices;
-	Checksum *Chk;
-	int Threads;
-	bool GPU;
-	unsigned char cmethod;
-	unsigned char fmethod;
 	unsigned int crc;
 	
 	public:
-	void Comp(); // Compress buffer
-	void Decomp(); // Decompress buffer
-	void InitComp(int bsize); // initialize compressor with blocksize bsize
-	void InitDecomp(int t, bool gpu); // Initialize decoder with t threads on either cpu or gpu
-	void Free(); // Release memory from the compressor or decompressor
+	void Comp(); 				// Compress buffer
+	void Decomp(); 				// Decompress buffer
+	void InitComp(Options Opt); 		// Initialize compressor with blocksize bsize
+	void InitDecomp(Options Opt); 		// Initialize decoder with t threads on either cpu or gpu
+	void Free(); 				// Release memory from the compressor or decompressor
 	
-	int CompReadBlock(FILE *in); // Read raw input to compressor
-	int DecompReadBlock(FILE *in); // Read compressed block to decompressor
-	void CompWriteBlock(FILE *out); // Write compressed contents to output
-	void DecompWriteBlock(FILE *out); // Write out extracted data
+	int CompReadBlock(FILE *in);		// Read raw input to compressor
+	int DecompReadBlock(FILE *in); 		// Read compressed block to decompressor
+	void CompWriteBlock(FILE *out); 	// Write compressed contents to output
+	void DecompWriteBlock(FILE *out); 	// Write out extracted data
 	
-	void DisplayHeaderContents(); // Only really used for debugging
+	void SwapStreams();			// Swap input stream with output stream
+	void DisplayHeaderContents(); 		// Only really used for debugging
 	
-	void Compress(FILE *in, FILE *out, int t, int bsize); // Compress input file to output file with t threads and blocksize bsize
-	void Decompress(FILE *in, FILE *out, int t, bool gpu); // Decompress input to output with t internal threads
+	void Compress(FILE *in, FILE *out, Options Opt); 	// Compress input file to output file
+	void Decompress(FILE *in, FILE *out, Options Opt); 	// Decompress input to output 
 };
 #endif // JAM_H //
