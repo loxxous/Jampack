5b6985863f2f6b7a3fb9bb05d332796da07b8278
loxxous <lucasgmarsh@gmail.com>
2017-09-19 18:53:11 -0500

    Update lpx.cpp

--- lpx.cpp
+++ lpx.cpp
@@ -4,89 +4,167 @@
 #include "lpx.hpp"
 
 /**
-* Update prefix table, count consecutive hits of suffixes with a single leading prefix
+* Update prefix table, count consecutive hits of order-n suffixes with a single leading prefix
+* If there's a lot of local matches, reduce the order and adjust the threshold for more optimal sort ordering.
+* If there's too many misses occuring, increase the order and adjust the threshold to prevent distortion. (This is all automatic)
 */
-void Lpx::UpdateTable(PrefixRecord *table, unsigned int *cxt, unsigned int pos)
+void Lpx::UpdateTable(PrefixRecord **table, unsigned int *cxt, unsigned int pos, unsigned char *order)
 {
-	unsigned int LP = *cxt >> 24;
-	unsigned int LS = *cxt & 0xffffff;
+	unsigned int LP = (*cxt >> (*order * 8)) & 0xff;
+	unsigned int LS = *cxt & ((1 << (*order * 8)) - 1);
+	int distance = (pos - table[*order - 1][LP].pos); 
 	
-	if(pos > 3)
+	int lower = MinThreshold;
+	int upper = 0;
+	
+	if(table[*order - 1][LP].hits < MaxThreshold)
+		upper = __max((distance), MinThreshold);
+	else
+		upper = __min((distance >> *order), MaxThreshold >> *order);
+		
+	int bound = (distance <= lower) ? lower : (distance > upper) ? upper : distance; 
+	
+	if(pos > *order)
 	{
-		if(table[LP].cxt == LS)
+		if(table[*order - 1][LP].cxt == LS)
 		{
-			table[LP].hits++;
+			table[*order - 1][LP].pos = pos - *order;
+			table[*order - 1][LP].hits++;
+			table[*order - 1][LP].miss = 0;
+		
+			if(table[*order - 1][LP].hits > (unsigned int)((table[*order - 1][LP].threshold << (*order)) << 3) && *order > 1 && *order <= 3)
+				order[0]--;
+			
+			if(table[*order - 1][LP].hits > (table[*order - 1][LP].threshold << 1) && table[*order - 1][LP].miss == 0)
+				table[*order - 1][LP].threshold += (bound - table[*order - 1][LP].threshold) >> *order;
 		}
 		else
 		{
-			table[LP].hits = 0;
-			table[LP].cxt = LS;
+			table[*order - 1][LP].hits >>= 2;
+			table[*order - 1][LP].miss++;
+			table[*order - 1][LP].cxt = LS;
+			
+			if(table[*order - 1][LP].miss > (table[*order - 1][LP].threshold * table[*order - 1][LP].threshold * *order) && *order >= 1 && *order < 3)
+				order[0]++;
+			
+			if(table[*order - 1][LP].miss > table[*order - 1][LP].threshold)
+				table[*order - 1][LP].threshold += (MaxThreshold - table[*order - 1][LP].threshold) >> (4 - *order); 
 		}
-		table[LP].pos = pos - 3;
 	}
 }
 
-void Lpx::Encode(Buffer Input, Buffer Output)
-{ 	
-	PrefixRecord *t = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
-	unsigned int cxt = 0;
-	const unsigned int mask = 0xff;
-	for (int i = 0; i < *Input.size;)
+void Lpx::EncodeBlock(unsigned char *input, unsigned char *output, Index len)
+{
+	PrefixRecord *table[3];
+	table[0] = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
+	table[1] = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
+	table[2] = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
+	if(table[0] == NULL || table[1] == NULL || table[2] == NULL )
+		Error("Could not allocate prefix model!");
+	for(int i = 0; i < 256; i++)
 	{
-		int dist = i - t[cxt & mask].pos; // Anchor prediction offset
-		if (t[cxt & mask].hits > HitThreshold && dist < MaxRecordSize)
+		table[0][i].threshold = MaxThreshold >> 1;
+		table[1][i].threshold = MaxThreshold >> 1;
+		table[2][i].threshold = MaxThreshold >> 1;
+	}
+	unsigned int cxt = 0;
+	unsigned char order = 3;
+	const unsigned int mask = 0xff;
+	for (int i = 0; i < len;)
+	{
+		unsigned int dist = i - table[order - 1][cxt & mask].pos; // Anchor prediction offset
+		if (table[order - 1][cxt & mask].hits > table[order - 1][cxt & mask].threshold && dist < MaxRecordSize)
 		{
 			unsigned char err;
 			do
 			{
-				Output.block[i] = err = Input.block[i - dist] ^ Input.block[i];
-				UpdateTable(t, &cxt, i);
-				cxt = (cxt << 8) | Input.block[i];
+				output[i] = err = input[i - dist] ^ input[i];
+				UpdateTable(table, &cxt, i, &order);
+				cxt = (cxt << 8) | input[i];
 				i++;
 			}
-			while (err == 0 && i < *Input.size);
+			while (err == 0 && i < len);
 		}
 		else
 		{
-			Output.block[i] = Input.block[i];
-			UpdateTable(t, &cxt, i);
-			cxt = (cxt << 8) | Input.block[i];
+			output[i] = input[i];
+			UpdateTable(table, &cxt, i, &order);
+			cxt = (cxt << 8) | input[i];
 			i++;
 		}
 	}
-	free(t);
+	free(table[0]);
+	free(table[1]);
+	free(table[2]);
+}
+
+void Lpx::DecodeBlock(unsigned char *input, unsigned char *output, Index len)
+{
+	PrefixRecord *table[3];
+	table[0] = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
+	table[1] = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
+	table[2] = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
+	if(table[0] == NULL || table[1] == NULL || table[2] == NULL )
+		Error("Could not allocate prefix model!");
+	for(int i = 0; i < 256; i++)
+	{
+		table[0][i].threshold = MaxThreshold >> 1;
+		table[1][i].threshold = MaxThreshold >> 1;
+		table[2][i].threshold = MaxThreshold >> 1;
+	}
+	unsigned int cxt = 0;
+	unsigned char order = 3;
+	const unsigned int mask = 0xff; 
+	for (int i = 0; i < len;)
+	{
+		unsigned int dist = i - table[order - 1][cxt & mask].pos; 
+		if (table[order - 1][cxt & mask].hits > table[order - 1][cxt & mask].threshold && dist < MaxRecordSize)
+		{
+			unsigned char err;
+			do
+			{
+				err = input[i];
+				output[i] = output[i - dist] ^ input[i];
+				UpdateTable(table, &cxt, i, &order);
+				cxt = (cxt << 8) | output[i];
+				i++;
+			}
+			while (err == 0 && i < len);
+		}
+		else
+		{
+			output[i] = input[i];
+			UpdateTable(table, &cxt, i, &order);
+			cxt = (cxt << 8) | output[i];
+			i++;
+		}
+	}
+
+	free(table[0]);
+	free(table[1]);
+	free(table[2]);
+}
+
+void Lpx::Encode(Buffer Input, Buffer Output, Options Opt)
+{
+	const Index PrefixBlock = *Input.size / 4; // Share workload amoung 4 threads
+	#pragma omp parallel for num_threads(Opt.Threads)
+	for(Index i = 0; i < *Input.size; i += PrefixBlock)
+	{
+		Index len = ((i + PrefixBlock) < *Input.size) ? PrefixBlock : (*Input.size - i);
+		EncodeBlock(&Input.block[i], &Output.block[i], len);
+	}
 	*Output.size = *Input.size;
 }
 
-void Lpx::Decode(Buffer Input, Buffer Output)
+void Lpx::Decode(Buffer Input, Buffer Output, Options Opt)
 {
-	PrefixRecord *t = (PrefixRecord*)calloc(256, sizeof(PrefixRecord));
-	unsigned int cxt = 0;
-	const unsigned int mask = 0xff; 
-	for (int i = 0; i < *Input.size;)
+	const Index PrefixBlock = *Input.size / 4; // Share workload amoung 4 threads
+	#pragma omp parallel for num_threads(Opt.Threads)
+	for(Index i = 0; i < *Input.size; i += PrefixBlock)
 	{
-		int dist = i - t[cxt & mask].pos; 
-		if (t[cxt & mask].hits > HitThreshold && dist < MaxRecordSize)
-		{
-			unsigned char err;
-			do
-			{
-				err = Input.block[i];
-				Output.block[i] = Output.block[i - dist] ^ Input.block[i];
-				UpdateTable(t, &cxt, i);
-				cxt = (cxt << 8) | Output.block[i];
-				i++;
-			}
-			while (err == 0 && i < *Input.size);
-		}
-		else
-		{
-			Output.block[i] = Input.block[i];
-			UpdateTable(t, &cxt, i);
-			cxt = (cxt << 8) | Output.block[i];
-			i++;
-		}
+		Index len = ((i + PrefixBlock) < *Input.size) ? PrefixBlock : (*Input.size - i);
+		DecodeBlock(&Input.block[i], &Output.block[i], len);
 	}
-	free(t);
 	*Output.size = *Input.size;
 }
