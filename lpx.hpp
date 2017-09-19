ad7cfe6fde5d7df373dc5c5a37b9224a63c89cfe
loxxous <lucasgmarsh@gmail.com>
2017-09-19 18:53:28 -0500

    Update lpx.hpp

--- lpx.hpp
+++ lpx.hpp
@@ -9,20 +9,28 @@
 #include "utils.hpp"
 
 class Lpx
-{	
+{
 private:
-	const int HitThreshold = 32; // Consecutive exact matches for a leading prefix
-	const int MaxRecordSize = 16 << 10; // Window for prefix to suffix correllations
+	const int MaxThreshold = 128; // Consecutive exact matches for a leading prefix
+	const int MinThreshold = 4;
+	const unsigned int MaxRecordSize = 64 << 10; // Window for prefix to suffix correllations
 	
 	struct PrefixRecord
 	{
 		unsigned int cxt;
 		unsigned int pos;
 		unsigned int hits;
+		unsigned int miss;
+		int threshold;
 	};
-	void UpdateTable(PrefixRecord *table, unsigned int *cxt, unsigned int pos);
+	
+	void UpdateTable(PrefixRecord **table, unsigned int *cxt, unsigned int pos, unsigned char *order);
+	void EncodeBlock(unsigned char *input, unsigned char *output, Index len);
+	void DecodeBlock(unsigned char *input, unsigned char *output, Index len);
+	void ReverseBlock(unsigned char *input, Index len);
+	
 public:
-	void Encode(Buffer Input, Buffer Output);
-	void Decode(Buffer Input, Buffer Output);
+	void Encode(Buffer Input, Buffer Output, Options Opt);
+	void Decode(Buffer Input, Buffer Output, Options Opt);
 };
 #endif // PREFIX_H
