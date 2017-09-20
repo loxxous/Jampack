1d0352e5d64e33783422b4935662061e21f1de20
loxxous <lucasgmarsh@gmail.com>
2017-09-19 19:12:36 -0500

    Add files via upload

---
+++ make32.bat
@@ -0,0 +1,3 @@
+g++ -std=c++14 -fopenmp -O3 ans.cpp bwt.cpp checksum.cpp cyclichhm.cpp divsufsort.cpp filters.cpp format.cpp jampack.cpp lpx.cpp lz77.cpp main.cpp model.cpp rank.cpp rle.cpp sys_detect.cpp utils.cpp -o Jampack_x86 -m32 -s -static
+PAUSE
+
